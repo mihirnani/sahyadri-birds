ca1215afa8c20ffa398effa1c4c202fc87099882
Mihir <67517581+mihirnani@users.noreply.github.com>
2026-08-22 21:37:56 +0530

    robots

--- sahyadri-birds-sw.js
+++ sahyadri-birds-sw.js
@@ -3,7 +3,7 @@
 "use strict";
 
 const PREFIX = "sahyadri-birds-";
-const VERSION = "v5";
+const VERSION = "v6";
 const SHELL_CACHE = `${PREFIX}shell-${VERSION}`;
 const IMAGE_CACHE = `${PREFIX}images-${VERSION}`;
 
